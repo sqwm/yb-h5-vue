0f59f31041a54db27348f9ea565ee85f837054c2
fangjianguo <fangjianguo@alipay.com>
2018-11-09 20:00:31 +0800

    添加我的页面

---
+++ src/components/profile-item.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="profile-item-container" @click="goPage">
+  <div class="profile-photo-wrap">
+    <img :src="profileInfo.profilePhoto || defaultProfilePhoto" />
+  </div>
+  <div class="profile-info-wrap">
+    {{profileInfo.name}}-{{profileInfo.mobile}}
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Provide, Prop, Vue } from 'vue-property-decorator';
+import defaultProfilePhoto from '@/../public/img/defaultProfilePhoto.png';
+
+@Component
+export default class Mine extends Vue {
+  @Prop() private profileInfo!: object;
+  @Provide() private defaultProfilePhoto: string = defaultProfilePhoto;
+
+  private goPage() {
+    this.$router.push({ name: 'userDetail' });
+  }
+}
+</script>
+
+<style lang="scss" >
+@import "../assets/css/common";
+.profile-item-container {
+  @include flexbox;
+  width: 100%;
+  height: px2rem(160px);
+  background-color: $bgfff;
+  margin: 10px 0;
+  justify-content: space-between;
+  align-items: center;
+
+  .profile-photo-wrap {
+    margin-left: 15px;
+
+    img {
+      height: px2rem(120px);
+      width: px2rem(120px);
+      border-radius: px2rem(120px);
+    }
+  }
+  .profile-info-wrap {
+    @include flexbox;
+    align-items: center;
+    font-size: px2rem(36px);
+    margin-right: 15px;
+  }
+}
+</style>
--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="mine">
-    我的
+    <profile-item :profileInfo="profileInfo"></profile-item>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Header from '@/components/Header.vue'; 
+import { State } from 'vuex-class';
+import { Component, Provide, Vue } from 'vue-property-decorator';
+import profileItem from '@/components/profile-item.vue';
 
 @Component({
   components: {
-    Header,
+    profileItem,
   },
 })
-export default class Mine extends Vue {}
+export default class Mine extends Vue {
+  @State('userInfo') private userInfo!: object;
+  @Provide() private profileInfo: object = {
+    profilePhoto: '', name: '用户名', mobile: '手机',
+  };
+
+  private created() {
+    this.profileInfo = this.userInfo;
+  }
+}
 </script>
 
 <style lang="scss">
-.mine {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+  .mine {
+    display: flex;
+    justify-content: center;
+  }
 </style>
